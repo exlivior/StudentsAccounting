007f5fd76cda62b728ae181e9341dbcaec622850
exlivior <exlivior@gmail.com>
2021-02-10 21:54:22 +0200

    Add project files.

---
+++ StudentsAccounting/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentsAccounting.Models;
+
+namespace StudentsAccounting.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=StudentsAccounting;Integrated Security=True");
+        }
+    }
+}
---
+++ StudentsAccounting/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StudentsAccounting.Models
+{
+    public class Student : IdentityUser
+    {
+    }
+}
